ac290b399bd2d6ac54e944bcb9cc752b339a735e
shumakovd <shumakov_d@hotmail.com>
2023-08-29 10:17:32 +0300

    Initial Commit

---
+++ Dropbox Media Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dropbox Media Viewer
+//
+//  Created by Shumakov Dmytro on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
